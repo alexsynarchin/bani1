0eb97943fdcc46ca1bf0f2a579249c2c7dd56f50
Alexey <alexsynarchin@gmail.com>
2025-02-11 10:02:22 +0500

    end booking in 12:00

--- app/Http/Controllers/Api/BookingController.php
+++ app/Http/Controllers/Api/BookingController.php
@@ -14,7 +14,7 @@ class BookingController extends Controller
         $today =  Carbon::now()->addHours(5);
         $dayOfWeek = $today->dayOfWeek;
         $date = $today;
-        $hours = 10;
+        $hours = 12;
         $time = (int) $today->format('H');
         if($dayOfWeek === 6 ) {
           $hours = 12;
